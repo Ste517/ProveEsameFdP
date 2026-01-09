d0f9081504d30c8dfa84fa0d203dc063523f937a
Ste517 <grakulino07@gmail.com>
2026-01-09 11:32:43 +0100

    Minor changes

--- Appello 1 2026/compito.cpp
+++ Appello 1 2026/compito.cpp
@@ -1,8 +1,20 @@
 #include "compito.h"
 #include <cstring>
 
+bool sort_farmaci(Farmaco* &lista_farmaci) {
+
+}
+
 CasaDiCura::CasaDiCura(const char nome[], int numeroOspiti) {
-    if (strlen(nome) > 20) {
+    if (strlen(nome) > 20 || strlen(nome) == 0) {
+        strcpy(this->nome, "Default name");
+    } else {
         strcpy(this->nome, nome);
     }
+    nOspiti = numeroOspiti;
+}
+
+ostream &operator<<(ostream &os, CasaDiCura cc) {
+    os << "Casa di Cura '" << cc.nome << "'\n";
+
 }
--- Appello 1 2026/compito.h
+++ Appello 1 2026/compito.h
@@ -3,9 +3,18 @@
 #include <iostream>
 using namespace std;
 
+struct Farmaco {
+    char nome[16];
+    unsigned short int orario_assunzione;
+    bool assunto;
+};
+
 class CasaDiCura {
     char nome[21];
+    int nOspiti;
+public:
     CasaDiCura(const char nome[], int numeroOspiti);
+    friend ostream& operator<<(ostream& os, CasaDiCura cc);
 };
 
 
